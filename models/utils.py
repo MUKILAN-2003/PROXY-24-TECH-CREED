665dbf175eee73db4ee96f07a4b53e003136529b
MUKILAN S <65155327+MUKILAN-2003@users.noreply.github.com>
2024-10-06 01:51:32 +0530

    Transscript and Translation Done Video

--- models/utils.py
+++ models/utils.py
@@ -11,6 +11,7 @@ import os
 
 # libraries for audio to english text
 # https://www.thepythoncode.com/article/using-speech-recognition-to-convert-speech-to-text-python
+
 # pip3 install SpeechRecognition pydub
 # https://www.arxiv-vanity.com/papers/1710.08969/
 
@@ -44,8 +45,6 @@ checkpoint = 'facebook/nllb-200-distilled-600M'
 
 model = AutoModelForSeq2SeqLM.from_pretrained(checkpoint)
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-
-captioner = pipeline("image-to-text",model="Salesforce/blip-image-captioning-base")
 
 common_language = {
     "Bengali": "ben_Beng",
@@ -99,6 +98,47 @@ def text_audio(translation_text, language,path):
     return path
 
 
+#path - path with a video filename
+# filename - only the videoname
+# audiofolder - denotes where the audio is stored
+
+def extract_video_audio(path, filename , audiofolder):
+    videoClip = VideoFileClip(path)
+    aud_filename = filename.split('.')[0] + '.wav'
+    videoClip.audio.write_audiofile(os.path.join(audiofolder, aud_filename))
+    aud_filepath = os.path.join(audiofolder, aud_filename)
+    return aud_filepath  # return a audio file path
+
+
+# videopath, audiopath, combinedvideopath - folderpath with a filename
+def combine_audio_video(videopath, audiopath, combinedvideopath):
+    videoclip = VideoFileClip(videopath)
+    audioclip = AudioFileClip(audiopath)
+    video = videoclip.set_audio(audioclip)
+    video.write_videofile(combinedvideopath)
+    return combinedvideopath
+
+
+# path of the audiofile with filename
+def englishimage_englishtext(path,target):
+    path = sys.path[0] + "/static/uploads/image/{0}".format(path)
+
+    caption = imageCaptioning(path)
+    source = dectLang(caption)
+
+    translated = text2textTranslation(source,target,caption)
+    return caption, translated
+
+# path of the audiofile with filename
+def englishimage_englishtext_tran(path,target):
+    path = sys.path[0] + "/static/uploads/image/{0}".format(path)
+
+    caption = imageCaptioning(path)
+
+    translated = englishtovernacular(lang=target,text=caption)
+    return caption, translated
+
+
 # path of the audiofile with filename
 def englishaudio_englishtext(path,target):
     """
@@ -129,28 +169,21 @@ def englishaudio_englishtext(path,target):
         # the `folder_name` directory.
         chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
         audio_chunk.export(chunk_filename, format="wav")
-
-        with sr.WavFile(chunk_filename) as source:              # use "test.wav" as the audio source
-            audio_listened = r.record(source)                        # extract audio data from the file
-
-        try:
-            list_pred = r.recognize(audio_listened,True)                  # generate a list of possible transcriptions
-            print("Possible transcriptions:")
-            for prediction in list_pred:
-                print(" " + prediction["text"] + " (" + str(prediction["confidence"]*100) + "%)")
-        
-            source = dectLang(text)
-        
-            text = f"{text.capitalize()}. "
-            whole_text += text2textTranslation(source,target,text)
-            
-            # return the text for all chunks detected
-            print(whole_text)
-            return whole_text
-
-        except LookupError:                                 # speech is unintelligible
-            print("Could not understand audio")
-
+        # recognize the chunk
+        with sr.AudioFile(chunk_filename) as source:
+            audio_listened = r.record(source)
+            # try converting it to text
+            try:
+                text = r.recognize_google(audio_listened)
+                source = dectLang(text)
+            except sr.UnknownValueError as e:
+                print("Error:", str(e))
+            else:
+                text = f"{text.capitalize()}. "
+                whole_text += text2textTranslation(source,target,text)
+    # return the text for all chunks detected
+    print(whole_text)
+    return whole_text
 
 def englishaudio_trans(path,target):
     """
@@ -182,7 +215,7 @@ def englishaudio_trans(path,target):
         chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
         audio_chunk.export(chunk_filename, format="wav")
         # recognize the chunk
-        with sr.WavFile(chunk_filename) as source:
+        with sr.AudioFile(chunk_filename) as source:
             audio_listened = r.record(source)
             # try converting it to text
             try:
@@ -195,6 +228,30 @@ def englishaudio_trans(path,target):
     # return the text for all chunks detected
     print(whole_text)
     return whole_text
+
+
+def getVideoId(url):
+    if url.find("watch") != -1:
+        video_id = url.split("=")[1]
+        return video_id
+    else:
+        video_id = url.split("be/")[1]
+        return video_id
+
+# return a text if transcription is available otherwise it return a empty text
+#  if any error occured it return none
+def youtube_translate(url,target):
+    try:
+        video_id = getVideoId(url)
+        transcript = YouTubeTranscriptApi.get_transcript(video_id)
+        result = ""
+        source = dectLang(transcript[0]["text"])
+        for frame in transcript:
+            result += (text2textTranslation(source, target, frame['text']) +" ")
+        return result
+    except Exception as e:
+        print(e)
+        return None
 
 # transliteration 
 
@@ -211,3 +268,32 @@ language = {
     "Telugu": "te",
     "Urdu": "ur",
 }
+
+# text - english text , lang- transliteration language name
+def englishtovernacular(text,lang):
+  out = english2vernacular.translit_sentence(text,lang_code = lang)
+  return out
+
+# text - vernacular text , lang - vernacular language name
+def vernaculartoenglish(text,lang):
+  lang_code = language[lang]
+  out = vernacular2english.translit_sentence(text,lang_code = lang_code)
+  return out
+
+
+def download_youtube_video(url, output_path='.'):
+    try:
+        # Create a YouTube object for the video URL
+        yt = pytube.YouTube(url)
+        
+        # Get the highest resolution stream (You can choose different streams as per your requirements)
+        stream = yt.streams.get_highest_resolution()
+        
+        # Download the video to the specified output path
+        filename = stream.download(output_path=output_path)
+        
+        return os.path.basename(filename)
+        
+    except Exception as e:
+        print("An error occurred:", str(e))
+        return None
--- views/views.py
+++ views/views.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import (Flask, flash, make_response, redirect, render_template,
+                   request, session, url_for)
+import secrets
+import bcrypt
 
 from app import *
-from models.utils import *
 
-import sys
+from models.utils import *
+from shutil import copyfile
 
 common_language = {
     "Bengali": "ben_Beng",
@@ -20,45 +23,248 @@ common_language = {
     "English": "eng_Latn"
 }
 
+@app.route('/test')
+def test():
+    return render_template('imagetotextin.html')
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    session["email"] = 'PROXY_TECHCREED'
+    return render_template('index.html')
 
 @app.route('/dashboard')
 def dashboard():
     return render_template('dashboard.html')
 
-
 @app.route('/text', methods=["POST", "GET"])
 def text():
-    if request.method == "POST":
-        lag = request.form.get("lag")
-        text = request.form.get("text") 
-        sourceLang = dectLang(text)
-        translatedText = text2textTranslation(source=sourceLang, target=lag, text=text)
-        return render_template('text.html', translatedText=translatedText,common_language = common_language)
-    return render_template('text.html',common_language = common_language)
+    if "email" in session:
+        if request.method == "POST":
+            lag = request.form.get("lag")
+            text = request.form.get("text") 
+            sourceLang = dectLang(text)
+            translatedText = text2textTranslation(source=sourceLang, target=lag, text=text)
+            return render_template('text.html', translatedText=translatedText,common_language = common_language)
+        return render_template('text.html',common_language = common_language)
+
+    else:
+        return redirect('/login')
+
+languageCode2Lang = {
+  "ben_Beng": "Bengali",
+  "guj_Gujr": "Gujarati",
+  "hin_Deva": "Hindi",
+  "kan_Knda": "Kannada",
+  "mal_Mlym": "Malayalam",
+  "mar_Deva": "Marathi",
+  "npi_Deva": "Nepali",
+  "sin_Sinh": "Sinhala",
+  "tam_Taml": "Tamil",
+  "tel_Telu": "Telugu",
+  "urd_Arab": "Urdu",
+  "eng_Latn": "English"
+}
+
+# input - video file and language
+# output - translated audio filepath
+@app.route('/video', methods=["POST", "GET"])
+def video():
+    if "email" in session:
+        
+        if request.method == "POST":
+            file = request.files['file']
+            language = request.form['lang']
+
+            if file.filename == '' :
+                flash('No image selected for uploading')
+                return redirect(request.url)   
+    
+            else:
+                audiofolder = os.path.join(app.config['UPLOAD_FOLDER'], 'audio')
+                translatedaudiofolder = os.path.join(app.config['UPLOAD_FOLDER'], 'translatedaudio')
+                videofolder = os.path.join(app.config['UPLOAD_FOLDER'], 'video')
+                videopath = os.path.join(videofolder, file.filename)
+                translatedaudiopath = os.path.join(translatedaudiofolder, file.filename)
+                combinedvideopath = os.path.join(app.config['UPLOAD_FOLDER'], 'combinedvideo',file.filename)
+                file.save(videopath)
+                audiopath = extract_video_audio(videopath,file.filename,audiofolder)
+                print(audiopath.split('/')[-1])
+                translated_text = englishaudio_englishtext(audiopath.split('/')[-1],language)
+                text_audio(translated_text,languageCode2Lang[language],translatedaudiopath)
+                combine_audio_video(videopath,translatedaudiopath,combinedvideopath)
+
+                return render_template('video.html', result=True, translated_text=translated_text, audipPath=audiopath.split('/')[-1], videoPath=file.filename, common_language = common_language)
+
+        return render_template('video.html', result=False, translated_text='', videoPath='', audipPath = '', common_language = common_language)
+
+    else:
+        return redirect('/login')
+
 
 # input - audio file and language
 # output - translated audio filepath
 @app.route('/audio', methods=["POST", "GET"])
 def audio():
-    if request.method == "POST":
-        file = request.files['file']
-        language = request.form['lang']
-        if file.filename == '' :
-            flash('No image selected for uploading')
-            return redirect(request.url)   
-
-        else:
-            audiopath = os.path.join(app.config['UPLOAD_FOLDER'], 'audio',file.filename)
-            translatedaudiofolder = os.path.join(app.config['UPLOAD_FOLDER'], 'translatedaudio')
-            translatedaudiopath = os.path.join(translatedaudiofolder, file.filename)
-            file.save(audiopath)
-            translated_text = englishaudio_englishtext(file.filename,language)
-            text_audio(translated_text,languageCode2Lang[language],translatedaudiopath)
-
-            return render_template('audio.html',common_language = common_language,result=True,  translated_text=translated_text,translatedaudiopath=file.filename)
+    if "email" in session:
         
+        if request.method == "POST":
+            file = request.files['file']
+            language = request.form['lang']
+            if file.filename == '' :
+                flash('No image selected for uploading')
+                return redirect(request.url)   
+    
+            else:
+                audiopath = os.path.join(app.config['UPLOAD_FOLDER'], 'audio',file.filename)
+                translatedaudiofolder = os.path.join(app.config['UPLOAD_FOLDER'], 'translatedaudio')
+                translatedaudiopath = os.path.join(translatedaudiofolder, file.filename)
+                file.save(audiopath)
+                translated_text = englishaudio_englishtext(file.filename,language)
+                text_audio(translated_text,languageCode2Lang[language],translatedaudiopath)
 
-    return render_template('audio.html',common_language = common_language,result=False,translated_text='',translatedaudiopath='')
+                return render_template('audio.html',common_language = common_language,result=True,  translated_text=translated_text,translatedaudiopath=file.filename)
+            
+
+        return render_template('audio.html',common_language = common_language,result=False,translated_text='',translatedaudiopath='')
+
+    else:
+        return redirect('/login')
+    
+
+# input - url and language
+# output - translated video filepath
+@app.route('/youtubevideo', methods=["POST", "GET"])
+def youtubevideo():
+    if "email" in session:
+        
+        if request.method == "POST":
+            url = ""
+            language = ""
+            if url == '' :
+                flash('No url is selected for translating')
+                return redirect(request.url)   
+    
+            else:
+                videofolder = os.path.join(app.config['UPLOAD_FOLDER'], 'video')
+                filename = download_youtube_video(url,videofolder)
+                audiofolder = os.path.join(app.config['UPLOAD_FOLDER'], 'audio')
+                translatedaudiofolder = os.path.join(app.config['UPLOAD_FOLDER'], 'translatedaudio')
+                videopath = os.path.join(videofolder, filename)
+                translatedaudiopath = os.path.join(translatedaudiofolder, filename)
+                combinedvideopath = os.path.join(app.config['UPLOAD_FOLDER'], 'combinedvideo', filename)
+                audiopath = extract_video_audio(videopath,filename,audiofolder)
+                translated_text = englishaudio_englishtext(audiopath,language)
+                text_audio(translated_text,languageCode2Lang[language],translatedaudiopath)
+                combine_audio_video(videopath,translatedaudiopath,combinedvideopath)
+
+                return combinedvideopath
+
+        return render_template('video.html')
+
+    else:
+        return redirect('/login')
+    
+
+transliterationlanguage = {
+    "Bengali": "bn",
+    "Gujarati": "gu",
+    "Hindi": "hi",
+    "Kannada": "kn",
+    "Malayalam": "ml",
+    "Marathi": "mr",
+    "Nepali": "ne",
+    "Sinhala": "si",
+    "Tamil": "ta",
+    "Telugu": "te",
+    "Urdu": "ur",
+}
+
+transliterationcodetolanguage = {
+    "bn": "Bengali",
+    "gu": "Gujarati",
+    "hi": "Hindi",
+    "kn": "Kannada",
+    "ml": "Malayalam",
+    "mr": "Marathi",
+    "ne": "Nepali",
+    "si": "Sinhala",
+    "ta": "Tamil",
+    "te": "Telugu",
+    "ur": "Urdu",
+}
+
+@app.route('/texttran', methods=["POST", "GET"])
+def texttran():
+    if "email" in session:
+        if request.method == "POST":
+            lag = request.form.get("lag")
+            text = request.form.get("text") 
+            translatedText = englishtovernacular(lang=lag, text=text)
+            return render_template('texttran.html', translatedText=translatedText,common_language = transliterationlanguage)
+        return render_template('texttran.html',common_language = transliterationlanguage)
+
+    else:
+        return redirect('/login')
+
+
+# input - video file and language
+# output - translated audio filepath
+@app.route('/videotran', methods=["POST", "GET"])
+def videotran():
+    if "email" in session:
+        
+        if request.method == "POST":
+            file = request.files['file']
+            language = request.form['lang']
+
+            if file.filename == '' :
+                flash('No image selected for uploading')
+                return redirect(request.url)   
+    
+            else:
+                audiofolder = os.path.join(app.config['UPLOAD_FOLDER'], 'audio')
+                translatedaudiofolder = os.path.join(app.config['UPLOAD_FOLDER'], 'translatedaudio')
+                videofolder = os.path.join(app.config['UPLOAD_FOLDER'], 'video')
+                videopath = os.path.join(videofolder, file.filename)
+                translatedaudiopath = os.path.join(translatedaudiofolder, file.filename)
+                combinedvideopath = os.path.join(app.config['UPLOAD_FOLDER'], 'combinedvideo',file.filename)
+                file.save(videopath)
+                audiopath = extract_video_audio(videopath,file.filename,audiofolder)
+                print(audiopath.split('/')[-1])
+                translated_text = englishaudio_trans(audiopath.split('/')[-1],language)
+                text_audio(translated_text,transliterationcodetolanguage[language],translatedaudiopath)
+                combine_audio_video(videopath,translatedaudiopath,combinedvideopath)
+
+                return render_template('videotran.html', result=True, translated_text=translated_text, audipPath=audiopath.split('\\')[-1], videoPath=file.filename, common_language = transliterationlanguage)
+
+        return render_template('videotran.html', result=False, translated_text='', videoPath='', audipPath = '', common_language = transliterationlanguage)
+
+    else:
+        return redirect('/login')
+    
+@app.route('/audiotran', methods=["POST", "GET"])
+def audiotran():
+    if "email" in session:
+        
+        if request.method == "POST":
+            file = request.files['file']
+            language = request.form['lang']
+            if file.filename == '' :
+                flash('No image selected for uploading')
+                return redirect(request.url)   
+    
+            else:
+                audiopath = os.path.join(app.config['UPLOAD_FOLDER'], 'audio',file.filename)
+                translatedaudiofolder = os.path.join(app.config['UPLOAD_FOLDER'], 'translatedaudio')
+                translatedaudiopath = os.path.join(translatedaudiofolder, file.filename)
+                file.save(audiopath)
+                translated_text = englishaudio_trans(file.filename,language)
+                text_audio(translated_text,transliterationcodetolanguage[language],translatedaudiopath)
+
+                return render_template('audiotran.html',common_language = transliterationlanguage,result=True,  translated_text=translated_text,translatedaudiopath=file.filename)
+            
+
+        return render_template('audiotran.html',common_language = transliterationlanguage,result=False,translated_text='',translatedaudiopath='')
+
+    else:
+        return redirect('/login')
